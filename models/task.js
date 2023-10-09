e08eb808f35afea2dc09ce89d760fdb6766d625f
K-Honsu <feranmia51@gmail.com>
2023-10-09 15:40:09 +0100

    feat: add task model schema

---
+++ models/task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema
+
+const TaskSchema = new schema({
+    user_id : {
+        type: mongoose.Schema.ObjectId,
+        ref : "users"
+    },
+    title : {type : String, required : true},
+    description : {type : String, required: true},
+    pending : {type : String, enum : ["pending", "completed"], default: "pending"},
+    created_at : {type: Date, default : new Date()},
+})
+
+
+const TaskModel = mongoose.model("tasks", TaskSchema)
+
+module.exports = TaskModel
