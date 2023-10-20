62440d0d129c72699b42a2dfcb43511394b6641a
K-Honsu <feranmia51@gmail.com>
2023-10-20 14:24:08 +0100

    fix: refactor autho service

--- auth-service/auth.js
+++ auth-service/auth.js
@@ -1,26 +1,15 @@
-// const { auth } = require("express-openid-connect")
-// require("dotenv").config()
-
-// const config = {
-//     authRequired : false,
-//     auth0Logout : true,
-//     secret : process.env.SECRET,
-//     baseURL: process.env.BASE_URL,
-//     clientID : process.env.CLIENTID,
-//     issuerBaseURL : process.env.ISSUER_BASE_URL
-// }
-
-// module.exports = auth(config)
-
-const { auth } = require('express-openid-connect');
+const { auth } = require("express-openid-connect")
+require("dotenv").config()
 
 const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'awertyujnjhgfdfghj',
-  baseURL: 'http://localhost:3005',
-  clientID: 'wm3L8e0FqKCL9TS6Ho3Ol4ATA1iiYHFL',
-  issuerBaseURL: 'https://dev-lq7m30jvn62zzyr4.us.auth0.com'
-};
+    authRequired : false,
+    auth0Logout : true,
+    secret : process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID : process.env.CLIENTID,
+    issuerBaseURL : process.env.ISSUER_BASE_URL
+}
 
 module.exports = auth(config)
+
+
